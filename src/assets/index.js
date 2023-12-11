42d5f8674e5bd6d5f3d1bae87c4db71da361ed8d
Daniel Bogale <104236560+danibog@users.noreply.github.com>
2023-12-11 22:46:44 +0300

    editing experience lists

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,6 +29,8 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import tripleBottomLine from "./company/3bl.png";
+import dasesa from "./company/dasesa.png";
 
 export {
   logo,
@@ -59,4 +61,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  tripleBottomLine,
+  dasesa,
 };
